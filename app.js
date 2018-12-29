7fbff1d3073f804124cb16f024d9393bf342c4d1
adishpatle1998 <adishpatle1998@gmail.com>
2018-12-29 22:29:42 +0530

    here it goes

---
+++ app.js
@@ -0,0 +1,54 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    seedDB      = require("./seeds"),
+    passport    = require("passport"),
+   LocalStrategy= require("passport-local"),
+    User        = require("./models/user"),
+ methodOverride = require("method-override")
+
+//Requiring routes 
+var campgroundRoutes  = require("./routes/campgrounds"),
+    commentsRoutes    = require("./routes/comments"),
+    indexRoutes       = require("./routes/index");
+    
+mongoose.connect("mongodb://localhost:27017/newdb",{useNewUrlParser: true},err=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("connect to db") 
+       }
+});
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+//removed as per new requirenments:):):)// seedDB();
+
+//Passport Configuration
+app.use(require("express-session")({
+    secret: "I was here....",
+    resave : false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+    res.locals.currentUser=req.user;
+    next();
+})
+
+app.use("/campgrounds",campgroundRoutes);
+app.use("/campgrounds/:id/comments",commentsRoutes);
+app.use(indexRoutes);
+
+app.listen(3000, function(){
+   console.log("The YelpCamp Server Has Started!");
+});
